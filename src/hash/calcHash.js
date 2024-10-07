b617bcf504f28c911603e1ca6ab83f28c53be9dd
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-08 01:02:27 +0300

    Update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+const crypto = require('crypto');
+let file = './files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
+
+    let x = fs.readFileSync(file);
+    console.log(x);
+    let y = crypto.createHash('sha256');
+    y.update(x);
+    const hex = y.digest('hex');
+    console.log(y,hex);
     // Write your code here 
 };
 
-await calculateHash();
+calculateHash();
