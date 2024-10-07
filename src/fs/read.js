567004e3d66c3f40ccfae6dec3bff27748259760
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:42:42 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const message = "FS operation failed";
+
 const read = async () => {
+    if(fs.existsSync('./files/fileToRead.txt')){
+        fs.readFile('./files/fileToRead.txt', 'utf8', (err, data) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log(data);
+        });
+    }else{
+        throw new Error(message);
+    }
     // Write your code here 
+
 };
 
-await read();
+read();
