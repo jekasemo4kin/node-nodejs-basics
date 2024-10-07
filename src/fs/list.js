49c599a3ccd8c85f7d7e4203f6c72e30a1317b31
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:42:33 +0300

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+const message = "FS operation failed";
+
 const list = async () => {
+    if(fs.existsSync('./files/')){
+        fs.readdir('./files/', function(err,elems){
+            if(err){
+                return console.log(err);
+            }
+            for (let elem of elems){
+                console.log('./files/'+elem);
+            }
+        })
+    }else{
+        throw new Error(message);
+    }
     // Write your code here 
+
 };
 
-await list();
+list();
