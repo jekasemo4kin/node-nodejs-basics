2adf1a1576b6e91af42d85983590ad7ca6403225
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:41:28 +0300

    Update env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
     // Write your code here 
+    //  в process.env записываются переменные, в консоли которые можно будет задавать/менять, нид только после этого писать node + файл, где сами переменные
+
+    Object.keys(process.env).forEach(function(key) {
+        console.log(  "RSS_"+ key + '=' + process.env[key] +';');
+    });  
+    console.log(213);
+    process.exit(1);
+    console.log(1111111);
 };
 
 parseEnv();
