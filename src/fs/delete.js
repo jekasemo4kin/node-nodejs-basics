b142626fb026b1e3b50c5689019f9faf248217a5
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:42:23 +0300

    Update delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+const fs = require('fs');
+let fileToRemove = 'fileToRemove.txt';
+const message = "FS operation failed";
+
+
 const remove = async () => {
+    if(fs.existsSync('./files/'+fileToRemove) ){
+        fs.unlink('./files/'+fileToRemove, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+        });
+    }else{
+        throw new Error(message); 
+    }
     // Write your code here 
+
 };
 
-await remove();
+remove();
