6bfa9bec084d66f3d2106542c314956ad868c887
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-08 02:35:42 +0300

    Update and rename compress.js to compress.cjs

---
+++ src/zip/compress.cjs
@@ -0,0 +1,12 @@
+const {createReadStream, createWriteStream} = require('fs');
+const createGzip = require('zlib');
+//                                                       './files/'
+
+const compress = async () => {
+    let x = createReadStream('./files/fileToCompress.txt');
+    x.pipe(createGzip()).pipe(createWriteStream('./files/archive.gz')).on('finish',()=>{console.log("Compression done",'./files/fileToCompress.txt')})
+
+    // Write your code here 
+};
+
+compress();
