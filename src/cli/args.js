3ee3fc097eef73516202dc8e5c22314dcfe62561
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:41:14 +0300

    Update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
     // Write your code here 
+    let args = process.argv.slice(2); // вырезаются строки node и название файла
+    let correctargs = args.map((item,index,array)=>{
+        if ([0,2].includes(index)){
+        return item.slice(2,item.length).concat(' is')}
+        if (index===1){return item.concat(',')}
+        return item});
+    console.log(correctargs.join(' '));
+
+
 };
 
 parseArgs();
