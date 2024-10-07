1e1ccba865be7436b1b97aa9963843fda8eb7f82
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:42:11 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+const fs = require('fs'); // подключение модуля файл системз. позволяет работать с файловой системой
+const path_to_file = "./src/fs/files/fresh.txt";
+const content = "I am fresh and young";
+const message = "FS operation failed";
+
 const create = async () => {
     // Write your code here 
+    
+    if(fs.existsSync('./files/fresh.txt')){
+        throw new Error(message); // ошибка, тк файл имеется
+    }else{
+        fs.writeFile('./files/fresh.txt', content, x =>{
+            console.log(x); 
+        })
+    }
+    
+
+
 };
 
-await create();
+
+
+create();
