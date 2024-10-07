170a322a210da90f2573498f8f17d5b9164fd14d
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-07 23:42:51 +0300

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+let wrongFilename = 'wrongFilename.txt';
+let properFilename = 'properFilename.md';
+const message = "FS operation failed";
+
+
+
 const rename = async () => {
     // Write your code here 
+    if (fs.existsSync('./files/'+wrongFilename) && !fs.existsSync('./files/'+properFilename)){
+        fs.rename('./files/'+wrongFilename, './files/'+properFilename, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+        });
+    }else{
+        throw new Error(message);
+    }
 };
 
-await rename();
+rename();
