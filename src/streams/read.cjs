7190979958234442cb8e7fdee2e42c19d99777b0
jekasemo4kin <95777961+jekasemo4kin@users.noreply.github.com>
2024-10-08 02:05:59 +0300

    Update and rename read.js to read.cjs

---
+++ src/streams/read.cjs
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+
+const read = async () => {
+
+    let readStream = fs.createReadStream('/files/','utf8');
+    readStream.on('data', function (chunk) {
+      console.log(chunk);
+    }).on('end', function () {
+      console.log('FINISHED! Everythn is OK!');
+    }).on('error', function () {
+      process.stderr.write(error);
+      process.exit(1);
+    });
+    // Write your code here 
+};
+
+ read();
